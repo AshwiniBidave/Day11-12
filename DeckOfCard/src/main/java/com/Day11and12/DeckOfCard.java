5ecc864b6c5e7d7aadf319e88f6fed99ea8d8713
AshwiniBidave <ashwinibidave1993@gmail.com>
2022-09-23 23:00:31 +0530

    first commit

---
+++ DeckOfCard/src/main/java/com/Day11and12/DeckOfCard.java
@@ -0,0 +1,13 @@
+package com.Day11and12;
+
+/**
+ * Hello world!
+ *
+ */
+public class DeckOfCard 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
